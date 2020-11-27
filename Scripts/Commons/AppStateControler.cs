628a739864ee233ceffeb0fcf86a00790060b239
mu-777 <water.tree.rainbow@gmail.com>
2020-11-27 19:20:47 +0900

    Add AppStateController

---
+++ Scripts/Commons/AppStateControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppStateControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Application.isPlaying)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            }
+
+        }
+    }
+}
