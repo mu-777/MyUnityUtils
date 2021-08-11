bad23e690f68234adc498ec09a16da5b6f3ac4ec
mu-777 <water.tree.rainbow@gmail.com>
2021-08-11 14:06:51 +0900

    Add attribute test

---
+++ Editor/ActiveSwitchAttribute.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ActiveSwitchAttribute : PropertyAttribute
+{
+    public readonly bool IsActive;
+    public ActiveSwitchAttribute(bool isActive)
+    {
+        this.IsActive = isActive;
+    }
+}
+
+[CustomPropertyDrawer(typeof(ActiveSwitchAttribute))]
+internal sealed class ActiveSwitchDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {     
+        var attr = base.attribute as ActiveSwitchAttribute;
+        EditorGUI.BeginDisabledGroup(!attr.IsActive);
+        EditorGUI.PropertyField(position, property, label);
+        EditorGUI.EndDisabledGroup();
+    }
+}
